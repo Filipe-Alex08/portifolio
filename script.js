f2238e7a3bd906cedcab3a0038f7295d1cc697fd
Filipe Alexandre <filipe.alex08@gmail.com>
2025-10-17 10:56:53 -0300

    Ajustando Layout

--- script.js
+++ script.js
@@ -38,3 +38,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 });
+
+
+// ---------- Menu Hamburguer ----------
+const menuToggle = document.getElementById("menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+
+menuToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
